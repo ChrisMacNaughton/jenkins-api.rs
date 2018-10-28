28b9bfdea1cb2beb33110196ed22208c78c1c439
Travis CI User <travis@example.org>
2018-10-28 22:48:07 +0000

    Documentation upload

---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["jenkins_api"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.80/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"jenkins_api/client/struct.TreeQueryParam.html\" title=\"struct jenkins_api::client::TreeQueryParam\">TreeQueryParam</a>",synthetic:false,types:["jenkins_api::client_internals::tree::TreeQueryParam"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
